19b43fdd6e0d8e61ce24d035ac6edeffa1df6faf
William Sharp <wsharp@ltu.edu>
2015-11-06 22:53:07 -0500

    tweaking the homepage to list posts

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,16 @@
 defmodule Potion.PageController do
   use Potion.Web, :controller
 
+  alias Potion.Repo
+  alias Potion.User
+
   def index(conn, _params) do
-    render conn, "index.html"
+    current_user = Plug.Conn.get_session(conn, :current_user)
+    posts = nil
+    if (current_user != nil) do
+      user = Repo.get!(User, current_user.id)
+      posts = Repo.all(assoc(user, :posts))
+    end
+    render(conn, "index.html", posts: posts)
   end
 end
