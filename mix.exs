06974308d56ad391b1ee989ee02c10528a696d85
William Sharp <wsharp@ltu.edu>
2015-11-08 17:52:49 -0500

    updating deps

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Potion.Mixfile do
   def project do
     [app: :potion,
      version: "0.0.1",
-     elixir: "~> 1.0",
+     elixir: "~> 1.1",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
@@ -35,7 +35,7 @@ defmodule Potion.Mixfile do
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:cowboy, "~> 1.0"},
+     {:cowboy, "~> 1.0.3"},
      {:comeonin, "~> 1.2.2"}]
   end
 
