65a0ecdd2648cb1c3cd5bc959ddc8328ab710158
William Sharp <wsharp@ltu.edu>
2015-11-09 20:36:40 -0500

    updating deps

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule Potion.Mixfile do
      {:phoenix_html, "~> 2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:cowboy, "~> 1.0.3"},
-     {:comeonin, "~> 1.2.2"}]
+     {:comeonin, "~> 1.4"}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
