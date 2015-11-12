b1734d26ec9c38ee0bd8ce3ceb9507fe4907eca1
William Sharp <wsharp@ltu.edu>
2015-11-11 20:15:38 -0500

    adding a code stripper

--- web/models/post.ex
+++ web/models/post.ex
@@ -22,5 +22,31 @@ defmodule Potion.Post do
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
+    |> strip_unsafe_body(params)
+  end
+
+  # private
+  defp strip_unsafe_body(model, %{"body" => nil}) do
+    model
+  end
+
+  defp strip_unsafe_body(model, %{"body" => body}) do
+    model |> put_change(:body, strip_tags(body))
+  end
+
+  defp strip_unsafe_body(model, _) do
+    model
+  end
+
+  defp strip_tags(body) do
+    body
+    |> strip_tag("script")
+    |> strip_tag("iframe")
+    |> strip_tag("link")
+  end
+
+  defp strip_tag(body, tag) do
+    strip_regex = ~r{<#{tag}[^>]*>[^<>]*(</#{tag}>)*}i
+    body |> String.replace(strip_regex, "")
   end
 end
--- web/views/post_view.ex
+++ web/views/post_view.ex
@@ -1,3 +1,9 @@
 defmodule Potion.PostView do
   use Potion.Web, :view
+
+  def markdown(body) do
+    body
+    |> Earmark.to_html
+    |> raw
+  end
 end
