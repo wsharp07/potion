f804f28481bb2f8d9bcd516f5909c123b6eabac4
William Sharp <wsharp@ltu.edu>
2015-11-21 16:55:45 -0500

    modifying styles

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -3,14 +3,15 @@ defmodule Potion.PageController do
 
   alias Potion.Repo
   alias Potion.User
+  alias Potion.Post
 
   def index(conn, _params) do
     current_user = Plug.Conn.get_session(conn, :current_user)
-    posts = nil
+    posts = Repo.all from p in Post, preload: [:user]
     if (current_user != nil) do
       user = Repo.get!(User, current_user.id)
-      posts = Repo.all(assoc(user, :posts))
     end
+
     render(conn, "index.html", posts: posts)
   end
 end
