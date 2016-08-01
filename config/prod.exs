fee61ec290a08272aaddc9f5651ea4bcc01a277f
William Sharp <wsharp@ltu.edu>
2016-07-31 21:15:18 -0700

    updating for heroku support

--- config/prod.exs
+++ config/prod.exs
@@ -13,9 +13,17 @@ use Mix.Config
 # which you typically run after static files are built.
 config :potion, Potion.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 8888],
+  url: [scheme: "https", host: "potion-1337.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   server: true
+
+config :potion, Potion.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -62,4 +70,4 @@ config :phoenix, :serve_endpoints, true
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+#import_config "prod.secret.exs"
